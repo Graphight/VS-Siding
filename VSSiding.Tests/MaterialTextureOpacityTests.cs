df684d6eeccd8e2b499125273e897d6f10b112df
Tom Marsh <tom.ian.marsh@gmail.com>
2026-09-18 21:39:36 -0400

    feat: daub finishes take the colour of the clay used

--- VSSiding.Tests/MaterialTextureOpacityTests.cs
+++ VSSiding.Tests/MaterialTextureOpacityTests.cs
@@ -83,6 +83,7 @@ public class MaterialTextureOpacityTests
             .Concat(Candidates(vintageStoryPath, "item", "itemtypes/resource/stonebrick.json"))
             .Concat(Candidates(vintageStoryPath, "item", "itemtypes/resource/stone.json"))
             .Concat(Candidates(vintageStoryPath, "item", "itemtypes/resource/burnedbrick.json"))
+            .Concat(Candidates(vintageStoryPath, "item", "itemtypes/resource/clay.json"))
             .ToList();
         var textureCodes = CollectTextureCodes(wallJson, "Framings", "FramingFamilies", candidates)
             .Concat(CollectTextureCodes(wallJson, "Infills", "InfillFamilies", candidates))
